571df44c51d8b8a4aeff56cde449d0322cf63a8f
javil <javil@LAPTOP-GMD6GUTE>
2024-11-20 11:33:30 +0100

    AppChat_v0.0_(Repositorio_Usuarios_Version_Inicial)

--- src/main/java/dominio/RepositorioUsuarios.java
+++ src/main/java/dominio/RepositorioUsuarios.java
@@ -2,8 +2,66 @@ package dominio;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dao.DAOException;
+import dao.FactoriaDAO;
+import dao.UsuarioDAO;
 
 public class RepositorioUsuarios {
-	private HashMap<Usuario, String> usuariosRegistrados;
-
+	
+	private Map<String,Usuario> usuariosRegistrados;
+	
+	private static RepositorioUsuarios unicaInstancia = new RepositorioUsuarios();
+	private FactoriaDAO factoria;
+	private UsuarioDAO daoUsuario;
+	
+	private RepositorioUsuarios() {
+		try {
+  			factoria = FactoriaDAO.getInstancia(FactoriaDAO.DAO_TDS);
+  			daoUsuario = factoria.getUsuarioDAO();
+  			usuariosRegistrados = new HashMap<String,Usuario>();
+  			this.cargarCatalogo();
+  		} catch (DAOException eDAO) {
+  			eDAO.printStackTrace();
+  		}
+	}
+	
+	public static RepositorioUsuarios getUnicaInstancia(){
+		return unicaInstancia;
+	}
+	
+	/*devuelve todos los usuarios*/
+	public List<Usuario> getUsuarios(){
+		ArrayList<Usuario> lista = new ArrayList<Usuario>();
+		for (Usuario u: usuariosRegistrados.values()) 
+			lista.add(u);
+		return lista;
+	}
+	
+	public Usuario getUsuario(int codigo) {
+		for (Usuario u: usuariosRegistrados.values()) {
+			if (u.getCodigo()==codigo) return u;
+		}
+		return null;
+	}
+	
+	public Usuario getUsuario(String movil) {
+		return usuariosRegistrados.get(movil); 
+	}
+	
+	public void addUsuario(Usuario usuario) {
+		usuariosRegistrados.put(usuario.getMovil(),usuario);
+	}
+	public void removeUsuario (Usuario usuario) {
+		usuariosRegistrados.remove(usuario.getMovil());
+	}
+	
+	/*Recupera todos los usuarios para trabajar con ellos en memoria*/
+	private void cargarCatalogo() throws DAOException {
+		 List<Usuario> usuariosBD = daoUsuario.recuperarTodosUsuarios();
+		 for (Usuario usuario: usuariosBD) 
+			     usuariosRegistrados.put(usuario.getMovil(),usuario);
+	}
 }
