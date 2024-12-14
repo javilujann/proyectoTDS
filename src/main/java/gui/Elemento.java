280b78bd9852ad0f71766e2add9a4813fadebefe
javil <javil@LAPTOP-GMD6GUTE>
2024-12-14 13:56:12 +0100

    Añadida la funcionalidad a elemento de mostrar el ultimo mensaje del chat con el contacto

--- src/main/java/gui/Elemento.java
+++ src/main/java/gui/Elemento.java
@@ -1,7 +1,11 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
+
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -10,6 +14,7 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 import dominio.Contacto;
+import dominio.Mensaje;
 
 
 
@@ -20,12 +25,15 @@ public class Elemento extends JPanel{
 	
 	public Elemento(Contacto _contacto) {
 		contacto = _contacto;
+		
+		//Ajustamos el Panel
 		this.setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
 		UtilsGui.fixSize(this,280,100);
 		this.setBackground(Color.LIGHT_GRAY);
 		this.setOpaque(true);
 		this.setBorder(new TitledBorder(contacto.getNombre()));
 		
+		//Añadimos la imagen
 		JLabel lblimagen=new JLabel();
 		Image imagen = contacto.getImage();
 		lblimagen.setIcon(new ImageIcon(imagen.getScaledInstance(75, 84, Image.SCALE_SMOOTH)));
@@ -33,8 +41,32 @@ public class Elemento extends JPanel{
 		lblimagen.setBorder(new LineBorder(Color.BLACK,2));
 		this.add(lblimagen);
 		
-		//Aqui la logica para mostrar el ultimo mensaje
-	}
+		// Añadimos el último mensaje
+        JPanel messagePanel = new JPanel(new BorderLayout());
+        messagePanel.setOpaque(false);
+        UtilsGui.fixSize(messagePanel, 180, 84); // Ajusta según sea necesario
+        
+        JLabel lastMessageLabel = new JLabel();
+        lastMessageLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        lastMessageLabel.setForeground(Color.DARK_GRAY);
+
+        // Obtener el último mensaje
+        String lastMessage = contacto.ultimoMensaje()
+                                     .map(Mensaje::getTexto)
+                                     .map(this::truncateMessage) // Truncar si es muy largo
+                                     .orElse("Sin mensajes");
+        
+        lastMessageLabel.setText("<html><i>" + lastMessage + "</i></html>");
+        messagePanel.add(lastMessageLabel, BorderLayout.CENTER);
+        
+        this.add(Box.createHorizontalStrut(10));
+        this.add(messagePanel);
+    }
+
+    private String truncateMessage(String mensaje) {
+        final int MAX_LENGTH = 50;
+        return mensaje.length() > MAX_LENGTH ? mensaje.substring(0, MAX_LENGTH) + "..." : mensaje;
+    }
 	
 	public Contacto getContacto() {
 		return contacto;
